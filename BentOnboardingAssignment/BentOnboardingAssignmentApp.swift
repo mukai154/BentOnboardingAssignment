ecb37a9d7624f2873bab82ee2e7566afa9b6f0c1
mukai154 <mukaiss@outlook.com>
2023-07-05 14:38:16 -0500

    Initial Commit

---
+++ BentOnboardingAssignment/BentOnboardingAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  BentOnboardingAssignmentApp.swift
+//  BentOnboardingAssignment
+//
+//  Created by Mukai Selekwa on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct BentOnboardingAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
